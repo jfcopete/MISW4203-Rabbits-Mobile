3e80a0ff176bcd20c65000c202773b3b6a7e56e9
Cristhian <ccdelgadop@gmail.com>
2024-04-28 12:09:32 -0500

    fix table

--- app/src/main/java/com/example/vinilos_rabbits/components/AlbumCard.kt
+++ app/src/main/java/com/example/vinilos_rabbits/components/AlbumCard.kt
@@ -3,11 +3,16 @@ package com.example.vinilos_rabbits.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 
@@ -21,13 +26,21 @@ fun AlbumCard(
         Surface(
             onClick=onAlbumDetails,
         ){
-            Column {
+            Column(
+            ) {
                 Image(
                     painter = rememberAsyncImagePainter(img),
                     contentDescription = name,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp)
                 )
-                Text(text = name)
-                Text(text = description)
+                Column(
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                ) {
+                    Text(text = name,)
+                    Text(text = description, )
+                }
             }
         }
 
