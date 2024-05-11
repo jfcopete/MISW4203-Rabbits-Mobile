51d1dff651421fd4bd41039dfe84e735829bc55c
JulioCesarForero <jc.foreroo1@uniandes.edu.co>
2024-05-10 20:04:45 -0500

    se ajusta el endpoint del Backend

--- app/src/main/java/com/example/vinilos_rabbits/services/VinilosApiService.kt
+++ app/src/main/java/com/example/vinilos_rabbits/services/VinilosApiService.kt
@@ -9,7 +9,7 @@ import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-private const val BASE_URL = "http://34.41.37.149:3000/"
+private const val BASE_URL = "http://34.42.218.64:3000/"
 
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
