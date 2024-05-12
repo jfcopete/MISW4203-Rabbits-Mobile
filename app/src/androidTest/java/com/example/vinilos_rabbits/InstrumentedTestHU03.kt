683bc4e2358ba8117b7990febf4e9689d3814d4d
Juan F Copete <jf.copete@uniandes.edu.co>
2024-05-12 13:45:44 -0500

    Se agrega tambien Test HU 03 Compelto

---
+++ app/src/androidTest/java/com/example/vinilos_rabbits/InstrumentedTestHU03.kt
@@ -0,0 +1,32 @@
+package com.example.vinilos_rabbits
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.example.vinilos_rabbits.activities.HomeApp
+import org.junit.Rule
+import org.junit.Test
+
+class InstrumentedTestHU03 {
+    @get:Rule
+    val rule = createComposeRule()
+
+    @Test
+    fun checkWelcomeTextDisplayed() {
+
+    }
+
+    @Test
+    fun checkArtistList() {
+
+        rule.setContent {
+            HomeApp()
+        }
+        rule.waitForIdle()
+        rule.onNodeWithContentDescription("Artista").performClick()
+        rule.onNodeWithText("Artista").assertExists()
+
+    }
+
+}
