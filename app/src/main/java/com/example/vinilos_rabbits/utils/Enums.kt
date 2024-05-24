a6afda17e12521ee1e9d583a887b1f982d938549
Jorge Ivan Puyo <ji.puyo@uniandes.edu.co>
2024-05-23 22:33:44 -0500

    Fix collector details title

--- app/src/main/java/com/example/vinilos_rabbits/utils/Enums.kt
+++ app/src/main/java/com/example/vinilos_rabbits/utils/Enums.kt
@@ -9,7 +9,7 @@ enum class VinilosScreen(@StringRes val title: Int) {
     Artist(title = R.string.artist),
     ArtistDetail(title = R.string.artist_detail_navbar_title),
     Collector(title = R.string.collector),
-    CollectorDetail(title = R.string.artist_detail_navbar_title),;
+    CollectorDetail(title = R.string.collector_detail_navbar_title),;
 
 }
 
